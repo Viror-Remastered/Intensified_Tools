b4159e3959576eac196bfe255dfcda1a031c1a0f
Viror-Remastered <wangzhen2352052@163.com>
2024-10-19 11:14:36 +0800

    Initial commit

---
+++ src/main/java/name/intensified_tools/IntensifiedToolsClient.java
@@ -0,0 +1,11 @@
+package name.intensified_tools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IntensifiedToolsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
